d7bcda5c2bae6a3588913d5629c0d5a0bc97d4c1
Thierry <thierry_192@outlook.com>
2020-11-16 21:17:14 -0300

    adjustment bug

--- src/helpers/request.helpers.js
+++ src/helpers/request.helpers.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const RequestClient = require("reqclient").RequestClient;
 
 // constant
-const request  = new RequestClient(process.env.QR_CODE_API_URL);
-const basePath = process.env.QR_CODE_API;
+// const request  = new RequestClient(process.env.QR_CODE_API_URL);
+// const basePath = process.env.QR_CODE_API;
 
 const requestHandler = async(body, res) => {
   try {
